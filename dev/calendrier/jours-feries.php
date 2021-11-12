cf5fac1ff792a39a57dbfa7438c03d2f687145e4
webeweb <support@webeweb.fr>
2021-11-12 11:39:08 +0100

    Add Jours fériés dev script

---
+++ dev/calendrier/jours-feries.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the gouv-api-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__ . "/../../vendor/autoload.php";
+
+use WBW\Library\GouvAPI\Calendrier\Provider\ApiProvider;
+use WBW\Library\GouvAPI\Calendrier\Request\JoursFeriesRequest;
+
+// Create the API provider.
+$provider = new ApiProvider();
+
+// Create a Jours fériés request.
+$request = new JoursFeriesRequest();
+$request->setZone(JoursFeriesRequest::ZONE_METROPOLE);
+$request->setAnnee(2021); // Optional
+
+// Call the API and get the response.
+$response = $provider->joursFeries($request);
+
+// Handle the response.
+$format = "%-5s: %s\n";
+
+foreach ($response->getJoursFeries() as $current) {
+
+    echo sprintf($format, "Date", $current->getDate()->format("d/m/Y"));
+    echo sprintf($format, "Nom", $current->getNom() . "\n");
+}
