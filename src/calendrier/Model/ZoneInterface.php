57469808506b4397eed008e6e1a8f31e647e0090
webeweb <support@webeweb.fr>
2021-11-12 11:37:22 +0100

    Add Zone interface

---
+++ src/calendrier/Model/ZoneInterface.php
@@ -0,0 +1,112 @@
+<?php
+
+/*
+ * This file is part of the gouv-api-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GouvAPI\Calendrier\Model;
+
+/**
+ * Zone interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GouvAPI\Calendrier\Model
+ */
+interface ZoneInterface {
+
+    /**
+     * Zone "Alsace Moselle".
+     *
+     * @var string
+     */
+    const ZONE_ALSACE_MOSELLE = "alsace-moselle";
+
+    /**
+     * Zone "Guadeloupe".
+     *
+     * @var string
+     */
+    const ZONE_GUADELOUPE = "guadeloupe";
+
+    /**
+     * Zone "Guyane".
+     *
+     * @var string
+     */
+    const ZONE_GUYANE = "guyane";
+
+    /**
+     * Zone "La Réunion".
+     *
+     * @var string
+     */
+    const ZONE_LA_REUNION = "la-reunion";
+
+    /**
+     * Zone "Martinique".
+     *
+     * @var string
+     */
+    const ZONE_MARTINIQUE = "martinique";
+
+    /**
+     * Zone "Mayotte".
+     *
+     * @var string
+     */
+    const ZONE_MAYOTTE = "mayotte";
+
+    /**
+     * Zone "Métropole".
+     *
+     * @var string
+     */
+    const ZONE_METROPOLE = "metropole";
+
+    /**
+     * Zone "Nouvelle Calédonie".
+     *
+     * @var string
+     */
+    const ZONE_NOUVELLE_CALEDONIE = "nouvelle-caledonie";
+
+    /**
+     * Zone "Polynésie Française".
+     *
+     * @var string
+     */
+    const ZONE_POLYNESIE_FRANCAISE = "polynesie-francaise";
+
+    /**
+     * Zone "Saint Barthélémy".
+     *
+     * @var string
+     */
+    const ZONE_SAINT_BARTHELEMY = "saint-barthelemy";
+
+    /**
+     * Zone "Saint Martin".
+     *
+     * @var string
+     */
+    const ZONE_SAINT_MARTIN = "saint-martin";
+
+    /**
+     * Zone "Saint Pierre et Miquelon".
+     *
+     * @var string
+     */
+    const ZONE_SAINT_PIERRE_MIQUELON = "saint-pierre-et-miquelon";
+
+    /**
+     * Zone "Wallis et Futuna".
+     *
+     * @var string
+     */
+    const ZONE_WALLIS_FUTUNA = "wallis-et-futuna";
+}
---
+++ tests/calendrier/Model/ZoneInterfaceTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the gouv-api-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GouvAPI\Calendrier\Tests\Model;
+
+use WBW\Library\GouvAPI\Calendrier\Model\ZoneInterface;
+use WBW\Library\GouvAPI\Calendrier\Tests\AbstractTestCase;
+
+/**
+ * Zone interface test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GouvAPI\Calendrier\Tests\Model
+ */
+class ZoneInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("alsace-moselle", ZoneInterface::ZONE_ALSACE_MOSELLE);
+        $this->assertEquals("guadeloupe", ZoneInterface::ZONE_GUADELOUPE);
+        $this->assertEquals("guyane", ZoneInterface::ZONE_GUYANE);
+        $this->assertEquals("la-reunion", ZoneInterface::ZONE_LA_REUNION);
+        $this->assertEquals("martinique", ZoneInterface::ZONE_MARTINIQUE);
+        $this->assertEquals("mayotte", ZoneInterface::ZONE_MAYOTTE);
+        $this->assertEquals("metropole", ZoneInterface::ZONE_METROPOLE);
+        $this->assertEquals("nouvelle-caledonie", ZoneInterface::ZONE_NOUVELLE_CALEDONIE);
+        $this->assertEquals("polynesie-francaise", ZoneInterface::ZONE_POLYNESIE_FRANCAISE);
+        $this->assertEquals("saint-barthelemy", ZoneInterface::ZONE_SAINT_BARTHELEMY);
+        $this->assertEquals("saint-martin", ZoneInterface::ZONE_SAINT_MARTIN);
+        $this->assertEquals("saint-pierre-et-miquelon", ZoneInterface::ZONE_SAINT_PIERRE_MIQUELON);
+        $this->assertEquals("wallis-et-futuna", ZoneInterface::ZONE_WALLIS_FUTUNA);
+    }
+}
