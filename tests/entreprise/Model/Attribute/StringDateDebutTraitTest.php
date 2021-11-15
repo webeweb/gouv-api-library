21c70b7513600a4e1ffb607c287255d233633102
webeweb <support@webeweb.fr>
2021-11-15 14:43:31 +0100

    Add string date début trait

---
+++ tests/entreprise/Model/Attribute/StringDateDebutTraitTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the gouv-api-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GouvAPI\Entreprise\Tests\Model\Attribute;
+
+use WBW\Library\GouvAPI\Entreprise\Tests\AbstractTestCase;
+use WBW\Library\GouvAPI\Entreprise\Tests\Fixtures\Model\Attribute\TestStringDateDebutTrait;
+
+/**
+ * String date début trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GouvAPI\Entreprise\Tests\Model\Attribute
+ */
+class StringDateDebutTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setDateDebut() method.
+     *
+     * @return void
+     */
+    public function testSetDateDebut(): void {
+
+        $obj = new TestStringDateDebutTrait();
+
+        $obj->setDateDebut("dateDebut");
+        $this->assertEquals("dateDebut", $obj->getDateDebut());
+    }
+
+}
