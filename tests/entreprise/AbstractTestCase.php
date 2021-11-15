83f5ae3abf5e220b8e1859cbbb091db2a527fff8
webeweb <support@webeweb.fr>
2021-11-15 14:45:15 +0100

    Add abstract test case

---
+++ tests/entreprise/AbstractTestCase.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the gouv-api-library package.
+ *
+ * (c) 2020 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GouvAPI\Entreprise\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Abstract test case.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GouvAPI\Entreprise\Tests
+ * @abstract
+ */
+abstract class AbstractTestCase extends TestCase {
+
+    /**
+     * Logger.
+     *
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp(): void {
+        parent::setUp();
+
+        // Set a Logger mock.
+        $this->logger = $this->getMockBuilder(LoggerInterface::class)->getMock();
+    }
+}
