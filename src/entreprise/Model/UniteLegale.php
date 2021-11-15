237f88542071864d3b4962211f96a611e1fe643f
webeweb <support@webeweb.fr>
2021-11-15 18:38:22 +0100

    Add Unité légale model

---
+++ src/entreprise/Model/UniteLegale.php
@@ -0,0 +1,631 @@
+<?php
+
+/*
+ * This file is part of the gouv-api-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GouvAPI\Entreprise\Model;
+
+use WBW\Library\GouvAPI\Common\Model\Attribute\StringNomTrait;
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\IntegerAnneeEffectifsTrait;
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\IntegerNombrePeriodesTrait;
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\StringActivitePrincipaleTrait;
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\StringCaractereEmployeurTrait;
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\StringCreatedAtTrait;
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\StringDateCreationTrait;
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\StringDateDebutTrait;
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\StringDateDernierTraitementTrait;
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\StringEtatAdministratifTrait;
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\StringNomenclatureActivitePrincipaleTrait;
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\StringSirenTrait;
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\StringStatutDiffusionTrait;
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\StringTrancheEffectifsTrait;
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\StringUpdatedAtTrait;
+use WBW\Library\Traits\Integers\IntegerIdTrait;
+
+/**
+ * Unité légale.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GouvAPI\Entreprise\Model
+ */
+class UniteLegale {
+
+    use IntegerIdTrait {
+        setId as public;
+    }
+    use IntegerAnneeEffectifsTrait;
+    use IntegerNombrePeriodesTrait;
+    use StringActivitePrincipaleTrait;
+    use StringCaractereEmployeurTrait;
+    use StringCreatedAtTrait;
+    use StringDateCreationTrait;
+    use StringDateDebutTrait;
+    use StringDateDernierTraitementTrait;
+    use StringEtatAdministratifTrait;
+    use StringNomenclatureActivitePrincipaleTrait;
+    use StringNomTrait;
+    use StringSirenTrait;
+    use StringStatutDiffusionTrait;
+    use StringTrancheEffectifsTrait;
+    use StringUpdatedAtTrait;
+
+    /**
+     * Année catégorie entreprise.
+     *
+     * @var string|null
+     */
+    protected $anneeCategorieEntreprise;
+
+    /**
+     * Catégorie entreprise.
+     *
+     * @var string|null
+     */
+    protected $categorieEntreprise;
+
+    /**
+     * Catégorie juridique.
+     *
+     * @var string|null
+     */
+    protected $categorieJuridique;
+
+    /**
+     * Date fin.
+     *
+     * @var string|null
+     */
+    protected $dateFin;
+
+    /**
+     * Dénomination.
+     *
+     * @var string|null
+     */
+    protected $denomination;
+
+    /**
+     * Dénomination usuelle 1.
+     *
+     * @var string|null
+     */
+    protected $denominationUsuelle1;
+
+    /**
+     * Dénomination usuelle 2.
+     *
+     * @var string|null
+     */
+    protected $denominationUsuelle2;
+
+    /**
+     * Dénomination usuelle 3.
+     *
+     * @var string|null
+     */
+    protected $denominationUsuelle3;
+
+    /**
+     * Economie sociale solidaire.
+     *
+     * @var bool|null
+     */
+    protected $economieSocialeSolidaire;
+
+    /**
+     * Identifiant association.
+     *
+     * @var string|null
+     */
+    protected $identifiantAssociation;
+
+    /**
+     * NIC siègge.
+     *
+     * @var string|null
+     */
+    protected $nicSiege;
+
+    /**
+     * Nom usage.
+     *
+     * @var string|null
+     */
+    protected $nomUsage;
+
+    /**
+     * Prénom 1.
+     *
+     * @var string|null
+     */
+    protected $prenom1;
+
+    /**
+     * Prénom 2.
+     *
+     * @var string|null
+     */
+    protected $prenom2;
+
+    /**
+     * Prénom 3.
+     *
+     * @var string|null
+     */
+    protected $prenom3;
+
+    /**
+     * Prénom 4.
+     *
+     * @var string|null
+     */
+    protected $prenom4;
+
+    /**
+     * Prénom usuel.
+     *
+     * @var string|null
+     */
+    protected $prenomUsuel;
+
+    /**
+     * Pseudonyme.
+     *
+     * @var string|null
+     */
+    protected $pseudonyme;
+
+    /**
+     * Sexe.
+     *
+     * @var string|null
+     */
+    protected $sexe;
+
+    /**
+     * Sigle.
+     *
+     * @var string|null
+     */
+    protected $sigle;
+
+    /**
+     * Unite purgée.
+     *
+     * @var bool|null
+     */
+    protected $unitePurgee;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the année catégorie entreprise.
+     *
+     * @return string|null Returns the année catégorie entreprise.
+     */
+    public function getAnneeCategorieEntreprise(): ?string {
+        return $this->anneeCategorieEntreprise;
+    }
+
+    /**
+     * Get the catégorie entreprise.
+     *
+     * @return string|null Returns the catégorie entreprise.
+     */
+    public function getCategorieEntreprise(): ?string {
+        return $this->categorieEntreprise;
+    }
+
+    /**
+     * Get the catégorie juridique.
+     *
+     * @return string|null Returns the catégorie juridique.
+     */
+    public function getCategorieJuridique(): ?string {
+        return $this->categorieJuridique;
+    }
+
+    /**
+     * Get the date fin.
+     *
+     * @return string|null Returns the date fin.
+     */
+    public function getDateFin(): ?string {
+        return $this->dateFin;
+    }
+
+    /**
+     * Get the dénomination.
+     *
+     * @return string|null Returns the dénomination.
+     */
+    public function getDenomination(): ?string {
+        return $this->denomination;
+    }
+
+    /**
+     * Get the dénomination usuelle 1.
+     *
+     * @return string|null Returns the dénomination usuelle 1.
+     */
+    public function getDenominationUsuelle1(): ?string {
+        return $this->denominationUsuelle1;
+    }
+
+    /**
+     * Get the dénomination usuelle 2.
+     *
+     * @return string|null Returns the dénomination usuelle 2.
+     */
+    public function getDenominationUsuelle2(): ?string {
+        return $this->denominationUsuelle2;
+    }
+
+    /**
+     * Get the dénomination usuelle 3.
+     *
+     * @return string|null Returns the dénomination usuelle 3.
+     */
+    public function getDenominationUsuelle3(): ?string {
+        return $this->denominationUsuelle3;
+    }
+
+    /**
+     * Get the économie sociale solidaire.
+     *
+     * @return bool|null Returns the économie sociale solidaire.
+     */
+    public function getEconomieSocialeSolidaire(): ?bool {
+        return $this->economieSocialeSolidaire;
+    }
+
+    /**
+     * Get the identifiant association.
+     *
+     * @return string|null Returns the identifiant association.
+     */
+    public function getIdentifiantAssociation(): ?string {
+        return $this->identifiantAssociation;
+    }
+
+    /**
+     * Get the NIC siège.
+     *
+     * @return string|null Returns the NIC siège.
+     */
+    public function getNicSiege(): ?string {
+        return $this->nicSiege;
+    }
+
+    /**
+     * Get the nom usage.
+     *
+     * @return string|null Returns the nom usage.
+     */
+    public function getNomUsage(): ?string {
+        return $this->nomUsage;
+    }
+
+    /**
+     * Get the prénom 1.
+     *
+     * @return string|null Returns the prénom 1.
+     */
+    public function getPrenom1(): ?string {
+        return $this->prenom1;
+    }
+
+    /**
+     * Get the prénom 2.
+     *
+     * @return string|null Returns the prénom 2.
+     */
+    public function getPrenom2(): ?string {
+        return $this->prenom2;
+    }
+
+    /**
+     * Get the prénom 3.
+     *
+     * @return string|null Returns the prénom 3.
+     */
+    public function getPrenom3(): ?string {
+        return $this->prenom3;
+    }
+
+    /**
+     * Get the prénom 4.
+     *
+     * @return string|null Returns the prénom 4.
+     */
+    public function getPrenom4(): ?string {
+        return $this->prenom4;
+    }
+
+    /**
+     * Get the prénom usuel.
+     *
+     * @return string|null Returns the prénom usuel.
+     */
+    public function getPrenomUsuel(): ?string {
+        return $this->prenomUsuel;
+    }
+
+    /**
+     * Get the pseudonyme.
+     *
+     * @return string|null Returns the pseudonyme.
+     */
+    public function getPseudonyme(): ?string {
+        return $this->pseudonyme;
+    }
+
+    /**
+     * Get the sexe.
+     *
+     * @return string|null Returns the sexe.
+     */
+    public function getSexe(): ?string {
+        return $this->sexe;
+    }
+
+    /**
+     * Get the sigle.
+     *
+     * @return string|null Returns the sigle.
+     */
+    public function getSigle(): ?string {
+        return $this->sigle;
+    }
+
+    /**
+     * Get the unité purgée.
+     *
+     * @return bool|null Returns the unité purgée.
+     */
+    public function getUnitePurgee(): ?bool {
+        return $this->unitePurgee;
+    }
+
+    /**
+     * Set the année catégorie entreprise.
+     *
+     * @param string|null $anneeCategorieEntreprise The année catégorie entreprise.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setAnneeCategorieEntreprise(?string $anneeCategorieEntreprise): UniteLegale {
+        $this->anneeCategorieEntreprise = $anneeCategorieEntreprise;
+        return $this;
+    }
+
+    /**
+     * Set the catégorie entreprise.
+     *
+     * @param string|null $categorieEntreprise The catégorie entreprise.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setCategorieEntreprise(?string $categorieEntreprise): UniteLegale {
+        $this->categorieEntreprise = $categorieEntreprise;
+        return $this;
+    }
+
+    /**
+     * Set the catégorie juridique.
+     *
+     * @param string|null $categorieJuridique The catégorie juridique.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setCategorieJuridique(?string $categorieJuridique): UniteLegale {
+        $this->categorieJuridique = $categorieJuridique;
+        return $this;
+    }
+
+    /**
+     * Set the date fin.
+     *
+     * @param string|null $dateFin The date fin.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setDateFin(?string $dateFin): UniteLegale {
+        $this->dateFin = $dateFin;
+        return $this;
+    }
+
+    /**
+     * Set the dénomination.
+     *
+     * @param string|null $denomination The dénomination.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setDenomination(?string $denomination): UniteLegale {
+        $this->denomination = $denomination;
+        return $this;
+    }
+
+    /**
+     * Set the dénomination usuelle 1.
+     *
+     * @param string|null $denominationUsuelle1 The dénomination usuelle 1.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setDenominationUsuelle1(?string $denominationUsuelle1): UniteLegale {
+        $this->denominationUsuelle1 = $denominationUsuelle1;
+        return $this;
+    }
+
+    /**
+     * Set the dénomination usuelle 2.
+     *
+     * @param string|null $denominationUsuelle2 The dénomination usuelle 2.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setDenominationUsuelle2(?string $denominationUsuelle2): UniteLegale {
+        $this->denominationUsuelle2 = $denominationUsuelle2;
+        return $this;
+    }
+
+    /**
+     * Set the dénomination usuelle 3.
+     *
+     * @param string|null $denominationUsuelle3 The dénomination usuelle 3.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setDenominationUsuelle3(?string $denominationUsuelle3): UniteLegale {
+        $this->denominationUsuelle3 = $denominationUsuelle3;
+        return $this;
+    }
+
+    /**
+     * Set the économie sociale solidaire.
+     *
+     * @param bool|null $economieSocialeSolidaire The économie sociale solidaire.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setEconomieSocialeSolidaire(?bool $economieSocialeSolidaire): UniteLegale {
+        $this->economieSocialeSolidaire = $economieSocialeSolidaire;
+        return $this;
+    }
+
+    /**
+     * Set the identifiant association.
+     *
+     * @param string|null $identifiantAssociation The identifiant association.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setIdentifiantAssociation(?string $identifiantAssociation): UniteLegale {
+        $this->identifiantAssociation = $identifiantAssociation;
+        return $this;
+    }
+
+    /**
+     * Set the NIC siège.
+     *
+     * @param string|null $nicSiege The NIC siège.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setNicSiege(?string $nicSiege): UniteLegale {
+        $this->nicSiege = $nicSiege;
+        return $this;
+    }
+
+    /**
+     * Set the nom usage.
+     *
+     * @param string|null $nomUsage The nom uasge.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setNomUsage(?string $nomUsage): UniteLegale {
+        $this->nomUsage = $nomUsage;
+        return $this;
+    }
+
+    /**
+     * Set the prénom 1.
+     *
+     * @param string|null $prenom1 The prénom 1.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setPrenom1(?string $prenom1): UniteLegale {
+        $this->prenom1 = $prenom1;
+        return $this;
+    }
+
+    /**
+     * Set the prénom 2.
+     *
+     * @param string|null $prenom2 The prénom 2.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setPrenom2(?string $prenom2): UniteLegale {
+        $this->prenom2 = $prenom2;
+        return $this;
+    }
+
+    /**
+     * Set the prénom 3.
+     *
+     * @param string|null $prenom3 The prénom 3.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setPrenom3(?string $prenom3): UniteLegale {
+        $this->prenom3 = $prenom3;
+        return $this;
+    }
+
+    /**
+     * Set the prénom 4.
+     *
+     * @param string|null $prenom4 The prénom 4.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setPrenom4(?string $prenom4): UniteLegale {
+        $this->prenom4 = $prenom4;
+        return $this;
+    }
+
+    /**
+     * Set the prnéom usuel.
+     *
+     * @param string|null $prenomUsuel The prénom usuel.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setPrenomUsuel(?string $prenomUsuel): UniteLegale {
+        $this->prenomUsuel = $prenomUsuel;
+        return $this;
+    }
+
+    /**
+     * Set the pseudonyme.
+     *
+     * @param string|null $pseudonyme The pseudonyme.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setPseudonyme(?string $pseudonyme): UniteLegale {
+        $this->pseudonyme = $pseudonyme;
+        return $this;
+    }
+
+    /**
+     * Set the sexe.
+     *
+     * @param string|null $sexe The sexe.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setSexe(?string $sexe): UniteLegale {
+        $this->sexe = $sexe;
+        return $this;
+    }
+
+    /**
+     * Set the sigle.
+     *
+     * @param string|null $sigle The sigle.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setSigle(?string $sigle): UniteLegale {
+        $this->sigle = $sigle;
+        return $this;
+    }
+
+    /**
+     * Set the unité purgée.
+     *
+     * @param bool|null $unitePurgee The unité purgée.
+     * @return UniteLegale Returns this unité légale.
+     */
+    public function setUnitePurgee(?bool $unitePurgee): UniteLegale {
+        $this->unitePurgee = $unitePurgee;
+        return $this;
+    }
+}
---
+++ tests/entreprise/Model/UniteLegaleTest.php
@@ -0,0 +1,345 @@
+<?php
+
+/*
+ * This file is part of the gouv-api-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GouvAPI\Entreprise\Tests\Model;
+
+use WBW\Library\GouvAPI\Entreprise\Model\UniteLegale;
+use WBW\Library\GouvAPI\Entreprise\Tests\AbstractTestCase;
+
+/**
+ * Unité légale test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GouvAPI\Entreprise\Tests\Model
+ */
+class UniteLegaleTest extends AbstractTestCase {
+
+    /**
+     * Tests the setAnneeCategorieEntreprise() method.
+     *
+     * @return void
+     */
+    public function testSetAnneeCategorieEntreprise(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setAnneeCategorieEntreprise("anneeCategorieEntreprise");
+        $this->assertEquals("anneeCategorieEntreprise", $obj->getAnneeCategorieEntreprise());
+    }
+
+    /**
+     * Tests the setCategorieEntreprise() method.
+     *
+     * @return void
+     */
+    public function testSetCategorieEntreprise(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setCategorieEntreprise("categorieEntreprise");
+        $this->assertEquals("categorieEntreprise", $obj->getCategorieEntreprise());
+    }
+
+    /**
+     * Tests the setCategorieJuridique() method.
+     *
+     * @return void
+     */
+    public function testSetCategorieJuridique(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setCategorieJuridique("categorieJuridique");
+        $this->assertEquals("categorieJuridique", $obj->getCategorieJuridique());
+    }
+
+    /**
+     * Tests the setDateFin() method.
+     *
+     * @return void
+     */
+    public function testSetDateFin(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setDateFin("dateFin");
+        $this->assertEquals("dateFin", $obj->getDateFin());
+    }
+
+    /**
+     * Tests the setDenomination() method.
+     *
+     * @return void
+     */
+    public function testSetDenomination(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setDenomination("denomination");
+        $this->assertEquals("denomination", $obj->getDenomination());
+    }
+
+    /**
+     * Tests the setDenominationUsuelle1() method.
+     *
+     * @return void
+     */
+    public function testSetDenominationUsuelle1(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setDenominationUsuelle1("denominationUsuelle1");
+        $this->assertEquals("denominationUsuelle1", $obj->getDenominationUsuelle1());
+    }
+
+    /**
+     * Tests the setDenominationUsuelle2() method.
+     *
+     * @return void
+     */
+    public function testSetDenominationUsuelle2(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setDenominationUsuelle2("denominationUsuelle2");
+        $this->assertEquals("denominationUsuelle2", $obj->getDenominationUsuelle2());
+    }
+
+    /**
+     * Tests the setDenominationUsuelle3() method.
+     *
+     * @return void
+     */
+    public function testSetDenominationUsuelle3(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setDenominationUsuelle3("denominationUsuelle3");
+        $this->assertEquals("denominationUsuelle3", $obj->getDenominationUsuelle3());
+    }
+
+    /**
+     * Tests the setEconomieSocialeSolidaire() method.
+     *
+     * @return void
+     */
+    public function testSetEconomieSocialeSolidaire(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setEconomieSocialeSolidaire(true);
+        $this->assertTrue($obj->getEconomieSocialeSolidaire());
+    }
+
+    /**
+     * Tests the setIdentifiantAssociation() method.
+     *
+     * @return void
+     */
+    public function testSetIdentifiantAssociation(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setIdentifiantAssociation("identifiantAssociation");
+        $this->assertEquals("identifiantAssociation", $obj->getIdentifiantAssociation());
+    }
+
+    /**
+     * Tests the setNicSiege() method.
+     *
+     * @return void
+     */
+    public function testSetNicSiege(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setNicSiege("nicSiege");
+        $this->assertEquals("nicSiege", $obj->getNicSiege());
+    }
+
+    /**
+     * Tests the setNomUsage() method.
+     *
+     * @return void
+     */
+    public function testSetNomUsage(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setNomUsage("nomUsage");
+        $this->assertEquals("nomUsage", $obj->getNomUsage());
+    }
+
+    /**
+     * Tests the setPrenom1() method.
+     *
+     * @return void
+     */
+    public function testSetPrenom1(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setPrenom1("prenom1");
+        $this->assertEquals("prenom1", $obj->getPrenom1());
+    }
+
+    /**
+     * Tests the setPrenom2() method.
+     *
+     * @return void
+     */
+    public function testSetPrenom2(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setPrenom2("prenom2");
+        $this->assertEquals("prenom2", $obj->getPrenom2());
+    }
+
+    /**
+     * Tests the setPrenom3() method.
+     *
+     * @return void
+     */
+    public function testSetPrenom3(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setPrenom3("prenom3");
+        $this->assertEquals("prenom3", $obj->getPrenom3());
+    }
+
+    /**
+     * Tests the setPrenom4() method.
+     *
+     * @return void
+     */
+    public function testSetPrenom4(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setPrenom4("prenom4");
+        $this->assertEquals("prenom4", $obj->getPrenom4());
+    }
+
+    /**
+     * Tests the setPrenomUsuel() method.
+     *
+     * @return void
+     */
+    public function testSetPrenomUsuel(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setPrenomUsuel("prenomUsuel");
+        $this->assertEquals("prenomUsuel", $obj->getPrenomUsuel());
+    }
+
+    /**
+     * Tests the setPseudonyme() method.
+     *
+     * @return void
+     */
+    public function testSetPseudonyme(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setPseudonyme("pseudonyme");
+        $this->assertEquals("pseudonyme", $obj->getPseudonyme());
+    }
+
+    /**
+     * Tests the setSexe() method.
+     *
+     * @return void
+     */
+    public function testSetSexe(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setSexe("sexe");
+        $this->assertEquals("sexe", $obj->getSexe());
+    }
+
+    /**
+     * Tests the setSigle() method.
+     *
+     * @return void
+     */
+    public function testSetSigle(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setSigle("sigle");
+        $this->assertEquals("sigle", $obj->getSigle());
+    }
+
+    /**
+     * Tests the setUnitePurgee() method.
+     *
+     * @return void
+     */
+    public function testSetUnitePurgee(): void {
+
+        $obj = new UniteLegale();
+
+        $obj->setUnitePurgee(true);
+        $this->assertTrue($obj->getUnitePurgee());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new UniteLegale();
+
+        $this->assertNull($obj->getId());
+        $this->assertNull($obj->getActivitePrincipale());
+        $this->assertNull($obj->getAnneeEffectifs());
+        $this->assertNull($obj->getCaractereEmployeur());
+        $this->assertNull($obj->getCreatedAt());
+        $this->assertNull($obj->getDateCreation());
+        $this->assertNull($obj->getDateDebut());
+        $this->assertNull($obj->getDateDernierTraitement());
+        $this->assertNull($obj->getEtatAdministratif());
+        $this->assertNull($obj->getNombrePeriodes());
+        $this->assertNull($obj->getNomenclatureActivitePrincipale());
+        $this->assertNull($obj->getSiren());
+        $this->assertNull($obj->getStatutDiffusion());
+        $this->assertNull($obj->getTrancheEffectifs());
+        $this->assertNull($obj->getUpdatedAt());
+
+        $this->assertNull($obj->getAnneeCategorieEntreprise());
+        $this->assertNull($obj->getCategorieEntreprise());
+        $this->assertNull($obj->getCategorieJuridique());
+        $this->assertNull($obj->getDateFin());
+        $this->assertNull($obj->getDenomination());
+        $this->assertNull($obj->getDenominationUsuelle1());
+        $this->assertNull($obj->getDenominationUsuelle2());
+        $this->assertNull($obj->getDenominationUsuelle3());
+        $this->assertNull($obj->getEconomieSocialeSolidaire());
+        $this->assertNull($obj->getIdentifiantAssociation());
+        $this->assertNull($obj->getNicSiege());
+        $this->assertNull($obj->getNomUsage());
+        $this->assertNull($obj->getPrenom1());
+        $this->assertNull($obj->getPrenom2());
+        $this->assertNull($obj->getPrenom3());
+        $this->assertNull($obj->getPrenom4());
+        $this->assertNull($obj->getPrenomUsuel());
+        $this->assertNull($obj->getPseudonyme());
+        $this->assertNull($obj->getSexe());
+        $this->assertNull($obj->getSigle());
+        $this->assertNull($obj->getUnitePurgee());
+    }
+}
