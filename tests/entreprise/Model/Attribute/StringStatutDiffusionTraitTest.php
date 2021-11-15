715337777f446f2b136d6ae49babbdd487cb2d95
webeweb <support@webeweb.fr>
2021-11-15 14:44:39 +0100

    Add string statut diffusion trait

---
+++ tests/entreprise/Model/Attribute/StringStatutDiffusionTraitTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the gouv-api-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GouvAPI\Entreprise\Tests\Model\Attribute;
+
+use WBW\Library\GouvAPI\Entreprise\Tests\AbstractTestCase;
+use WBW\Library\GouvAPI\Entreprise\Tests\Fixtures\Model\Attribute\TestStringStatutDiffusionTrait;
+
+/**
+ * String statut diffusion trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GouvAPI\Entreprise\Tests\Model\Attribute
+ */
+class StringStatutDiffusionTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setStatutDiffusion() method.
+     *
+     * @return void
+     */
+    public function testSetStatutDiffusion(): void {
+
+        $obj = new TestStringStatutDiffusionTrait();
+
+        $obj->setStatutDiffusion("statutDiffusion");
+        $this->assertEquals("statutDiffusion", $obj->getStatutDiffusion());
+    }
+
+}
