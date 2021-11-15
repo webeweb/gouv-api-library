2443cfcb09ddc2246bc9f7fc74eb6b9db3144257
webeweb <support@webeweb.fr>
2021-11-15 14:42:25 +0100

    Add integer nombre périodes trait

---
+++ tests/entreprise/Model/Attribute/IntegerNombrePeriodesTraitTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the gouv-api-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GouvAPI\Entreprise\Tests\Model\Attribute;
+
+use WBW\Library\GouvAPI\Entreprise\Tests\AbstractTestCase;
+use WBW\Library\GouvAPI\Entreprise\Tests\Fixtures\Model\Attribute\TestIntegerNombrePeriodesTrait;
+
+/**
+ * Integer nombre périodes trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GouvAPI\Entreprise\Tests\Model\Attribute
+ */
+class IntegerNombrePeriodesTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setNombrePeriodes() method.
+     *
+     * @return void
+     */
+    public function testSetNombrePeriodes(): void {
+
+        $obj = new TestIntegerNombrePeriodesTrait();
+
+        $obj->setNombrePeriodes(4);
+        $this->assertEquals(4, $obj->getNombrePeriodes());
+    }
+
+}
