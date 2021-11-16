f3ed2873e29d9a392e52af380cabe1138214cd48
webeweb <support@webeweb.fr>
2021-11-16 10:43:41 +0100

    Add Unités légales request

---
+++ src/entreprise/Request/UnitesLegalesRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the gouv-api-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GouvAPI\Entreprise\Request;
+
+use WBW\Library\GouvAPI\Entreprise\Model\Attribute\StringSirenTrait;
+use WBW\Library\Provider\API\SubstituableRequestInterface;
+
+/**
+ * Unités légales request.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GouvAPI\Entreprise\Request
+ */
+class UnitesLegalesRequest extends AbstractRequest implements SubstituableRequestInterface {
+
+    use StringSirenTrait;
+
+    /**
+     * Resource path.
+     *
+     * @avr string
+     */
+    const RESOURCE_PATH = "/api/sirene/v:version/unites_legales:siren";
+
+    /**
+     * Constructor.
+     *
+     * @param string|null $siren The SIREN.
+     */
+    public function __construct(string $siren = null) {
+        $this->setSiren($siren);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getResourcePath(): string {
+        return self::RESOURCE_PATH;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSubstituables(): array {
+        return [
+            ":siren" => null !== $this->getSiren() ? "/{$this->getSiren()}" : "",
+        ];
+    }
+}
---
+++ tests/entreprise/Request/UnitesLegalesRequestTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the gouv-api-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GouvAPI\Entreprise\Tests\Request;
+
+use WBW\Library\GouvAPI\Entreprise\Request\UnitesLegalesRequest;
+use WBW\Library\GouvAPI\Entreprise\Tests\AbstractTestCase;
+use WBW\Library\Provider\API\SubstituableRequestInterface;
+
+/**
+ * Unités légales request test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\GouvAPI\Entreprise\Tests\Request
+ */
+class UnitesLegalesRequestTest extends AbstractTestCase {
+
+    /**
+     * Tests the getSubstituables() method.
+     *
+     * @return void
+     */
+    public function testGetSubstituables(): void {
+
+        $obj = new UnitesLegalesRequest();
+
+        $this->assertEquals([":siren" => null], $obj->getSubstituables());
+
+        $obj->setSiren("350273462");
+        $this->assertEquals([":siren" => "/350273462"], $obj->getSubstituables());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $this->assertEquals("/api/sirene/v:version/unites_legales:siren", UnitesLegalesRequest::RESOURCE_PATH);
+
+        $obj = new UnitesLegalesRequest();
+
+        $this->assertInstanceOf(SubstituableRequestInterface::class, $obj);
+
+        $this->assertEquals(UnitesLegalesRequest::RESOURCE_PATH, $obj->getResourcePath());
+
+        $this->assertNull($obj->getSiren());
+    }
+}
