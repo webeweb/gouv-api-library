42814dfb0a3bbbf045a35603246c42e0d9cbb685
webeweb <support@webeweb.fr>
2024-03-17 15:15:17 +0100

    Add Feature collection response

---
+++ src/adresse/Response/FeatureCollectionResponse.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the gouv-api-library package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GouvApi\Adresse\Response;
+
+use WBW\Library\GeoJson\Model\FeatureCollection;
+use WBW\Library\GouvApi\Common\Response\AbstractResponse as BaseResponse;
+
+/**
+ * Feature collection response.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\GouvApi\Adresse\Response
+ */
+class FeatureCollectionResponse extends BaseResponse {
+
+    /**
+     * Feature collection.
+     *
+     * @var FeatureCollection|null
+     */
+    protected $featureCollection;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the feature collection.
+     *
+     * @return FeatureCollection|null Returns the feature collection.
+     */
+    public function getFeatureCollection(): ?FeatureCollection {
+        return $this->featureCollection;
+    }
+
+    /**
+     * Set the feature collection
+     *
+     * @param FeatureCollection|null $featureCollection
+     * @return $this
+     */
+    public function setFeatureCollection(?FeatureCollection $featureCollection): FeatureCollectionResponse {
+        $this->featureCollection = $featureCollection;
+        return $this;
+    }
+}
---
+++ tests/adresse/Response/FeatureCollectionResponseTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the gouv-api-library package.
+ *
+ * (c) 2024 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\GouvApi\Adresse\Tests\Response;
+
+use WBW\Library\GeoJson\Model\FeatureCollection;
+use WBW\Library\GouvApi\Adresse\Response\FeatureCollectionResponse;
+use WBW\Library\GouvApi\Adresse\Tests\AbstractTestCase;
+
+/**
+ * Feature collection response test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\GouvApi\Adresse\Tests\Response
+ */
+class FeatureCollectionResponseTest extends AbstractTestCase {
+
+    /**
+     * Test setFeatureCollection()
+     *
+     * @return void
+     */
+    public function testSetFeatureCollection(): void {
+
+        // Set a Feature collection mock.
+        $featureCollection = new FeatureCollection();
+
+        $obj = new FeatureCollectionResponse();
+
+        $obj->setFeatureCollection($featureCollection);
+        $this->assertSame($featureCollection, $obj->getFeatureCollection());
+
+    }
+}
